48bfc665fc43bae296d68d71b72c97cf50048876
PStewart1 <61563756+PStewart1@users.noreply.github.com>
2020-04-16 20:48:04 -0500

    added data access entities, and basic outline for repo, interface, and web api

---
+++ DataAccess/Entities/TicketContext.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataAccess.Entities
+{
+    public partial class TicketContext : DbContext
+    {
+        public TicketContext()
+        {
+        }
+
+        public TicketContext(DbContextOptions<TicketContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admins> Admins { get; set; }
+        public virtual DbSet<Comments> Comments { get; set; }
+        public virtual DbSet<Stores> Stores { get; set; }
+        public virtual DbSet<Tickets> Tickets { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:rev-stewart.database.windows.net,1433;Initial Catalog=Project2;Persist Security Info=False;User ID=Pstewart;Password=Sherlocked221;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admins>(entity =>
+            {
+                entity.Property(e => e.CurrentTicket).HasMaxLength(20);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Comments>(entity =>
+            {
+                entity.Property(e => e.Comment)
+                    .IsRequired()
+                    .HasMaxLength(280);
+
+                entity.Property(e => e.Datetime).HasColumnType("datetime");
+
+                entity.HasOne(d => d.Admin)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.AdminId)
+                    .HasConstraintName("FK__Comments__AdminI__5535A963");
+
+                entity.HasOne(d => d.Ticket)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.TicketId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Comments__Ticket__5441852A");
+            });
+
+            modelBuilder.Entity<Stores>(entity =>
+            {
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.State)
+                    .IsRequired()
+                    .HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<Tickets>(entity =>
+            {
+                entity.Property(e => e.Category)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Completed).HasMaxLength(5);
+
+                entity.Property(e => e.DatetimeClosed).HasColumnType("datetime");
+
+                entity.Property(e => e.DatetimeModified).HasColumnType("datetime");
+
+                entity.Property(e => e.DatetimeOpened).HasColumnType("datetime");
+
+                entity.Property(e => e.Deadline).HasColumnType("datetime");
+
+                entity.Property(e => e.Details)
+                    .IsRequired()
+                    .HasMaxLength(280);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasOne(d => d.Admin)
+                    .WithMany(p => p.Tickets)
+                    .HasForeignKey(d => d.AdminId)
+                    .HasConstraintName("FK__Tickets__AdminId__5070F446");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Tickets)
+                    .HasForeignKey(d => d.StoreId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Tickets__StoreId__5165187F");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Tickets)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Tickets__UserId__4F7CD00D");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.City).HasMaxLength(50);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.State).HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DataAccess/Repositories/TicketRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using DataAccess.Interfaces;
+
+namespace DataAccess.Repositories
+{
+    public class TicketRepo : ITicketRepo
+    {
+        private readonly TicketContext _dbContext;
+        private readonly ILogger<TicketRepo> _logger;
+
+        public TicketRepo(TicketContext dbContext, ILogger<TicketRepo> logger)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+
+
+
+    }
+}
